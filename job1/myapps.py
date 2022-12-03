d586975e66af9f46e3b4dd92eaae88c5b73d3ee6
gadido30 <gadido30@gmail.com>
2022-12-04 00:42:44 +0200

    Update myapps.py

--- job1/myapps.py
+++ job1/myapps.py
@@ -3,11 +3,15 @@ import docker
 #Connect to Docker Engine
 cli = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
 containers = cli.containers.list(all=True)
+containers_name = ""
+for i in containers:
+        containers_name += i.name + ","
+        print(i.name, i)
 app = Flask(__name__)
 #Return continers list
 @app.route('/')
 def list:
-    return "Continers List:" + containers
+    return "Continers List:" + containers_name
 
 if __name == '__main__':
     app.run(debug=True, host='0.0.0.0')
