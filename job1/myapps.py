3923fc8f2a90efff11dc96967a3ac6d52b5b3bd2
Gadi Dor <gadido30@gmail.com>
2022-12-04 00:14:05 +0200

    3 jenkins dsl piplines

---
+++ job1/myapps.py
@@ -0,0 +1,10 @@
+from flask import Flask
+           import docker
+           #Connect to Docker Engine
+           cli = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
+           containers = cli.containers.list(all=True)
+           app = Flask(__name__)
+           #Return continers list
+           @app.route('/')
+           def list():
+               return "Continers List:" + containers
